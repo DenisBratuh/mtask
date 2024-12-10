1ef67bbf8e4abe156d7a05e0167f6466aaf8ba55
Denys Bratukh <denis.bratuh@gmail.com>
2024-12-10 20:08:45 +0200

    removed unused constructors

--- src/main/java/com/example/mtask/exceptions/LogoDeletionException.java
+++ src/main/java/com/example/mtask/exceptions/LogoDeletionException.java
@@ -1,9 +1,6 @@
 package com.example.mtask.exceptions;
 
 public class LogoDeletionException extends RuntimeException {
-    public LogoDeletionException(String message) {
-        super(message);
-    }
 
     public LogoDeletionException(String message, Throwable cause) {
         super(message, cause);
--- src/main/java/com/example/mtask/exceptions/LogoDownloadException.java
+++ src/main/java/com/example/mtask/exceptions/LogoDownloadException.java
@@ -1,9 +1,6 @@
 package com.example.mtask.exceptions;
 
 public class LogoDownloadException extends RuntimeException {
-    public LogoDownloadException(String message) {
-        super(message);
-    }
 
     public LogoDownloadException(String message, Throwable cause) {
         super(message, cause);
