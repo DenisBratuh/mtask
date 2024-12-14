ffb43228b57ddd3b6807e2e441681060d703dfe0
Denys Bratukh <denis.bratuh@gmail.com>
2024-12-14 20:02:03 +0200

    * tests refactoring

--- src/test/java/com/example/mtask/utils/ImageUtilsTest.java
+++ src/test/java/com/example/mtask/utils/ImageUtilsTest.java
@@ -1,36 +1,37 @@
 package com.example.mtask.utils;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import com.example.mtask.enums.LogoType;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockMultipartFile;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class ImageUtilsTest {
+
+    private static final MockMultipartFile VALID_IMAGE_FILE =
+            new MockMultipartFile("image", "image.png", "image/png", new byte[0]);
+    private static final MockMultipartFile INVALID_FILE_TYPE =
+            new MockMultipartFile("file", "text.txt", "text/plain", new byte[0]);
+
+    private static final String VALID_FILE_NAME = "file.png";
 
     @Test
     void testCheckForImage_ValidImage() {
-        var file = new MockMultipartFile("image", "image.png", "image/png", new byte[0]);
-
-        assertDoesNotThrow(() -> ImageUtils.checkForImage(file));
+        assertDoesNotThrow(() -> ImageUtils.checkForImage(VALID_IMAGE_FILE));
     }
 
     @Test
     void testCheckForImage_InvalidFileType() {
-        var file = new MockMultipartFile("file", "text.txt", "text/plain", new byte[0]);
-
         var exception = assertThrows(
-            IllegalArgumentException.class, 
-            () -> ImageUtils.checkForImage(file)
+                IllegalArgumentException.class,
+                () -> ImageUtils.checkForImage(INVALID_FILE_TYPE)
         );
         assertEquals("Only image files are allowed.", exception.getMessage());
     }
 
     @Test
     void testGenerateObjectName_ValidInputs() {
-        var file = new MockMultipartFile("image", "image.png", "image/png", new byte[0]);
-
-        var objectName = ImageUtils.generateObjectName(file, LogoType.PRODUCT);
+        var objectName = ImageUtils.generateObjectName(VALID_IMAGE_FILE, LogoType.PRODUCT);
 
         assertTrue(objectName.startsWith("product/"));
         assertTrue(objectName.contains("image.png"));
@@ -38,16 +39,14 @@ class ImageUtilsTest {
 
     @Test
     void testValidateArguments_ValidInputs() {
-        String fileName = "file.png";
-
-        assertDoesNotThrow(() -> ImageUtils.validateFileName(fileName));
+        assertDoesNotThrow(() -> ImageUtils.validateFileName(VALID_FILE_NAME));
     }
 
     @Test
     void testValidateArguments_NullFileName() {
         var exception = assertThrows(
-            IllegalArgumentException.class, 
-            () -> ImageUtils.validateFileName(null)
+                IllegalArgumentException.class,
+                () -> ImageUtils.validateFileName(null)
         );
         assertEquals("File name cannot be null or empty.", exception.getMessage());
     }
